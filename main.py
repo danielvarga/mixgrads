92b2ab8d6b9797d8d4031cc6dac6d8a3f08119ae
Daniel Varga <daniel@renyi.hu>
2023-05-13 16:26:28 +0000

    replay

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+# loosely based on https://github.com/pytorch/examples/blob/main/mnist/main.py
+
 import argparse
+import copy
+import random
+import pickle
 import numpy as np
 import torch
 import torch.nn as nn
@@ -24,14 +29,25 @@ class Net(nn.Module):
         x = self.conv2(x)
         x = F.relu(x)
         x = F.max_pool2d(x, 2)
-        x = self.dropout1(x)
+        # x = self.dropout1(x)
         x = torch.flatten(x, 1)
         x = self.fc1(x)
         x = F.relu(x)
-        x = self.dropout2(x)
+        # x = self.dropout2(x)
         x = self.fc2(x)
         output = F.log_softmax(x, dim=1)
         return output
+
+
+def save_params(model):
+    params = []
+    for param in model.parameters():
+        params.append(param.clone().detach().cpu())
+    return params
+
+
+def set_params(model):
+    model.load_state_dict(sd)
 
 
 def save_grads(model):
@@ -58,6 +74,7 @@ def train(args, model, device, train_loader, optimizer, epoch):
     full_grad_save = []
     model.half()
     model.train()
+    initial_state_dict = copy.deepcopy(model.state_dict())
     for batch_idx, (data, target) in enumerate(train_loader):
         # it should be 89% by now:
         # if batch_idx == 100: break
@@ -75,8 +92,58 @@ def train(args, model, device, train_loader, optimizer, epoch):
                 100. * batch_idx / len(train_loader), loss.item()))
             if args.dry_run:
                 break
+    final_state_dict = copy.deepcopy(model.state_dict())
     print(len(full_grad_save), "gradients collected")
-    grad_tables = merge_grads(full_grad_save)
+    # grad_tables = merge_grads(full_grad_save)
+    full_record = (initial_state_dict, final_state_dict, full_grad_save)
+    with open("record.pkl", "wb") as f:
+        pickle.dump(full_record, f)
+
+
+# almost the same as test(), should refactor
+def evaluate(model, device, test_loader, n=1e9):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    evaluated = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            evaluated += len(data)
+            data, target = data.to(device).half(), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+            if evaluated >= n:
+                break
+
+    test_loss /= evaluated
+    test_acc = 100 * correct / evaluated
+    return (test_loss, test_acc)
+
+
+def apply_gradients(model, moment_state, gradients, lr, moment):
+    device = "cuda"
+    for param_tensor, moment_tensor, gradient_tensor in zip(model.parameters(), moment_state, gradients):
+        moment_tensor *= moment
+        moment_tensor -= lr * gradient_tensor.to(device)
+        param_tensor += moment_tensor
+
+
+def replay(model, test_loader, initial_state_dict, final_state_dict, full_grad_save, lr, moment):
+    device = "cuda"
+    log_interval = 50
+    model.half()
+    model.train()
+    model.load_state_dict(initial_state_dict)
+    first_gradients = full_grad_save[0]
+    moment_state = [torch.Tensor(np.zeros_like(param)).to(device) for param in first_gradients]
+    with torch.no_grad():
+        for batch_idx, gradients in enumerate(full_grad_save):
+            apply_gradients(model, moment_state, gradients, lr, moment)
+            if batch_idx % log_interval == 0:
+                test_loss, test_acc = evaluate(model, device, test_loader)
+                print(f"batch_idx: {batch_idx}\tloss: {test_loss:.6f}\tacc: {test_acc:.6f}")
 
 
 def test(model, device, test_loader):
@@ -96,6 +163,20 @@ def test(model, device, test_loader):
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
+
+
+def create_loaders(train_kwargs, test_kwargs):
+    transform=transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+        ])
+    dataset1 = datasets.MNIST('../data', train=True, download=True,
+                       transform=transform)
+    dataset2 = datasets.MNIST('../data', train=False,
+                       transform=transform)
+    train_loader = torch.utils.data.DataLoader(dataset1, **train_kwargs)
+    test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
+    return train_loader, test_loader
 
 
 def main():
@@ -141,20 +222,11 @@ def main():
         train_kwargs.update(cuda_kwargs)
         test_kwargs.update(cuda_kwargs)
 
-    transform=transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,))
-        ])
-    dataset1 = datasets.MNIST('../data', train=True, download=True,
-                       transform=transform)
-    dataset2 = datasets.MNIST('../data', train=False,
-                       transform=transform)
-    train_loader = torch.utils.data.DataLoader(dataset1, **train_kwargs)
-    test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
+    train_loader, test_loader = create_loaders(train_kwargs, test_kwargs)
 
     model = Net().to(device)
     # optimizer = optim.Adadelta(model.parameters(), lr=1.0)
-    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+    optimizer = optim.SGD(model.parameters(), lr=0.01, moment=0.9)
 
     # scheduler = StepLR(optimizer, step_size=1, gamma=0.7)
     for epoch in range(1, args.epochs + 1):
@@ -166,6 +238,28 @@ def main():
         torch.save(model.state_dict(), "mnist_cnn.pt")
 
 
+def main_replay():
+    # probably not worth loading to gpu, but cpu does not have float16.
+    device = "cuda"
+    model = Net().to(device)
+    with open("record.pkl", "rb") as f:
+        full_record = pickle.load(f)
+    (initial_state_dict, final_state_dict, full_grad_save) = full_record
+    print("full gradient replay read from pickle")
+
+    random.shuffle(full_grad_save)
+
+    batch_size = 64 # should be in sync with the way the pickle was built
+    lr = 0.01
+    moment = 0.9
+
+    train_kwargs = test_kwargs = {'batch_size': batch_size}
+    train_loader, test_loader = create_loaders(train_kwargs, test_kwargs)
+
+    replay(model, test_loader, initial_state_dict, final_state_dict, full_grad_save, lr, moment)
+
+
 if __name__ == '__main__':
-    main()
+    # main() ; exit()
+    main_replay() ; exit()
 
