0a0c933415f89391e26b6dbea9a8b5204c5e5e38
Daniel Varga <daniel@renyi.hu>
2023-05-13 16:27:22 +0000

    lenet

--- main.py
+++ main.py
@@ -39,6 +39,40 @@ class Net(nn.Module):
         return output
 
 
+# stolen from https://github.com/lychengrex/LeNet-5-Implementation-Using-Pytorch/blob/master/LeNet-5%20Implementation%20Using%20Pytorch.ipynb
+class LeNet(nn.Module):
+    # network structure
+    def __init__(self):
+        super(LeNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5, padding=2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1   = nn.Linear(16*5*5, 120)
+        self.fc2   = nn.Linear(120, 84)
+        self.fc3   = nn.Linear(84, 10)
+
+    def forward(self, x):
+        '''
+        One forward pass through the network.
+
+        Args:
+            x: input
+        '''
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2))
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        x = F.log_softmax(x, dim=1)
+        return x
+
+    def num_flat_features(self, x):
+        '''
+        Get the number of features in a batch of tensors `x`.
+        '''
+        size = x.size()[1:]
+        return np.prod(size)
+
 def save_params(model):
     params = []
     for param in model.parameters():
@@ -70,7 +104,7 @@ def merge_grads(full_grad_save):
     return grad_tables
 
 
-def train(args, model, device, train_loader, optimizer, epoch):
+def train(model, device, train_loader, optimizer, epoch):
     full_grad_save = []
     model.half()
     model.train()
@@ -86,12 +120,10 @@ def train(args, model, device, train_loader, optimizer, epoch):
         gradients = save_grads(model)
         full_grad_save.append(gradients)
         optimizer.step()
-        if batch_idx % args.log_interval == 0:
+        if batch_idx % 50 == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
-            if args.dry_run:
-                break
     final_state_dict = copy.deepcopy(model.state_dict())
     print(len(full_grad_save), "gradients collected")
     # grad_tables = merge_grads(full_grad_save)
@@ -181,47 +213,38 @@ def create_loaders(train_kwargs, test_kwargs):
     return train_loader, test_loader
 
 
-def pickle_filename(bs, lr, moment):
-    return f"record.bs{bs}_lr{lr}_mom{moment}.pkl"
+def pickle_filename(nick, bs, lr, moment):
+    return f"{nick}.bs{bs}_lr{lr}_mom{moment}.pkl"
 
 
-def main():
-    # Training settings
-    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                        help='input batch size for training (default: 64)')
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                        help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=1, metavar='N',
-                        help='number of epochs to train (default changed to 1)')
-    parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='disables CUDA training')
-    parser.add_argument('--no-mps', action='store_true', default=False,
-                        help='disables macOS GPU training')
-    parser.add_argument('--dry-run', action='store_true', default=False,
-                        help='quickly check a single pass')
-    parser.add_argument('--seed', type=int, default=1, metavar='S',
-                        help='random seed (default: 1)')
-    parser.add_argument('--log-interval', type=int, default=50, metavar='N',
-                        help='how many batches to wait before logging training status')
-    parser.add_argument('--save-model', action='store_true', default=False,
-                        help='For Saving the current Model')
-    args = parser.parse_args()
-    use_cuda = not args.no_cuda and torch.cuda.is_available()
-    use_mps = not args.no_mps and torch.backends.mps.is_available()
 
-    torch.manual_seed(args.seed)
 
-    if use_cuda:
-        device = torch.device("cuda")
-    elif use_mps:
-        device = torch.device("mps")
-    else:
-        device = torch.device("cpu")
+# this is where we set up everything:
 
-    train_kwargs = {'batch_size': args.batch_size}
-    test_kwargs = {'batch_size': args.test_batch_size}
-    if use_cuda:
+batch_size = 64 # should be in sync with the way the pickle was built
+
+lr = 0.08 ; moment = 0.0
+# lr = 0.04 ; moment = 0.1
+# lr = 0.02 ; moment = 0.5
+# lr = 0.01 ; moment = 0.9
+
+# nick = "record"
+nick = "lenet"
+
+# task = "train"
+task = "replay"
+do_shuffling = True
+
+device = "cuda"
+
+
+def main_train():
+    torch.manual_seed(1)
+    epochs = 1
+
+    train_kwargs = {'batch_size': batch_size}
+    test_kwargs = {'batch_size': batch_size}
+    if device == "cuda":
         cuda_kwargs = {'num_workers': 1,
                        'pin_memory': True,
                        'shuffle': True}
@@ -230,41 +253,39 @@ def main():
 
     train_loader, test_loader = create_loaders(train_kwargs, test_kwargs)
 
-    lr = 0.01
-    moment = 0.9
+    model = LeNet().to(device)
+    parameter_count = 0
+    for param in model.parameters():
+        print(tuple(param.shape))
+        parameter_count += torch.numel(param)
+    print("model parameter size", parameter_count)
 
-    model = Net().to(device)
     # optimizer = optim.Adadelta(model.parameters(), lr=1.0)
     optimizer = optim.SGD(model.parameters(), lr=lr, momentum=moment)
 
     # scheduler = StepLR(optimizer, step_size=1, gamma=0.7)
-    for epoch in range(1, args.epochs + 1):
-        full_record = train(args, model, device, train_loader, optimizer, epoch)
+    for epoch in range(1, epochs + 1):
+        full_record = train(model, device, train_loader, optimizer, epoch)
         test(model, device, test_loader)
         # scheduler.step()
 
-    if args.save_model:
-        torch.save(model.state_dict(), "mnist_cnn.pt")
-
-    filename = pickle_filename(args.batch_size, lr, moment)
+    filename = pickle_filename("lenet", batch_size, lr, moment)
     print("saving gradient records to", filename)
     with open(filename, "wb") as f:
         pickle.dump(full_record, f)
 
 
 def main_replay():
-    batch_size = 64 # should be in sync with the way the pickle was built
-    # lr = 0.08 ; moment = 0.0
-    # lr = 0.04 ; moment = 0.1
-    # lr = 0.02 ; moment = 0.5
-    lr = 0.01 ; moment = 0.9
-    do_shuffling = True
-
-    filename = pickle_filename(batch_size, lr, moment)
+    filename = pickle_filename(nick, batch_size, lr, moment)
 
     # probably not worth loading to gpu, but cpu does not have float16.
     device = "cuda"
-    model = Net().to(device)
+
+    if nick == "lenet":
+        model = LeNet().to(device)
+    elif nick == "record": # name for hysterical raisins.
+        model = Net().to(device)
+
     with open(filename, "rb") as f:
         full_record = pickle.load(f)
     (initial_state_dict, final_state_dict, full_grad_save) = full_record
@@ -289,6 +310,9 @@ def main_replay():
 
 
 if __name__ == '__main__':
-    # main() ; exit()
-    main_replay() ; exit()
-
+    if task == "train":
+        main_train()
+    elif task == "replay":
+        main_replay()
+    else:
+        assert False, f"unknown task {task}"
